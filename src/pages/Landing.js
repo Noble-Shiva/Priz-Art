bb178020bee58d3a0493a0d861ece98f40f8a037
ShivaKumar Reddy L <l.shivkumarreddy@gmail.com>
2020-10-29 15:21:54 +0530

    Updating Pages

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -27,30 +27,26 @@ class Landing extends Component {
                         </div>
                     </div>
                 </nav> */}
-                
+
                 <section>
-                    <div style={{width: '100%', height: '100vh', overflow: 'hidden', backgroundImage:"url(" + "https://source.unsplash.com/LAaSoL0LrYs/1920x1080" + ")", backgroundSize:'cover'}}>
-                        <div style={{paddingTop: 'calc(100vh - 30vh)', color: 'white'}}>
+                    <div className="bg">
+                        <div style={{ paddingTop: 'calc(100vh - 20vh)', color: 'black' }}>
                             <nav className="navbar navbar-light">
                                 <div className="container">
-                                    <a className="navbar-brand" href="/" style={{color: 'white', fontWeight: 'bold', fontSize: '30px'}}>PrizArt</a>
-                                    <div className="expand navbar-expand" id="navbarResponsive">
+                                    <a className="navbar-brand" href="/" style={{ color: 'black', fontWeight: 'bold', fontSize: '30px' }}>PrizArt</a>
+                                    <div className="collapse navbar-collapse">
                                         <ul className="navbar-nav ml-auto">
-                                            <li className="nav-item active" style={{padding: '10px 10px 10px 0px', textDecoration: 'none'}}>
-                                                {/* <a className="nav-link" href="/gallery" style={{color: 'white', fontWeight: 'bold'}}>Gallery</a> */}
-                                                <Link to="/gallery" style={{color: 'white', fontWeight: 'bold'}}>Gallery</Link>
+                                            <li className="nav-item active" style={{ padding: '10px 10px 10px 0px', textDecoration: 'none' }}>
+                                                <Link to="/gallery" style={{ color: 'black', fontWeight: 'bold' }}>Gallery</Link>
                                             </li>
-                                            <li className="nav-item" style={{padding: '10px', textDecoration: 'none'}}>
-                                                {/* <a className="nav-link" href="/about" style={{color: 'white', fontWeight: 'bold'}}>About</a> */}
-                                                <Link to="/" style={{color: 'white', fontWeight: 'bold'}}>About</Link>
+                                            <li className="nav-item" style={{ padding: '10px', textDecoration: 'none' }}>
+                                                <Link to="/" style={{ color: 'black', fontWeight: 'bold' }}>About</Link>
                                             </li>
-                                            <li className="nav-item" style={{padding: '10px', textDecoration: 'none'}}>
-                                                {/* <a className="nav-link" href="#" style={{color: 'white', fontWeight: 'bold'}}>Contact</a> */}
-                                                <Link to="/" style={{color: 'white', fontWeight: 'bold'}}>Contact</Link>
+                                            <li className="nav-item" style={{ padding: '10px', textDecoration: 'none' }}>
+                                                <Link to="/" style={{ color: 'black', fontWeight: 'bold' }}>Contact</Link>
                                             </li>
-                                            <li className="nav-item" style={{padding: '10px', textDecoration: 'none'}}>
-                                                {/* <a className="nav-link" href="#" style={{color: 'white', fontWeight: 'bold'}}>Admin</a> */}
-                                                <Link to="/" style={{color: 'white', fontWeight: 'bold'}}>Admin</Link>
+                                            <li className="nav-item" style={{ padding: '10px', textDecoration: 'none' }}>
+                                                <Link to="/" style={{ color: 'black', fontWeight: 'bold' }}>Admin</Link>
                                             </li>
                                         </ul>
                                     </div>
@@ -58,8 +54,6 @@ class Landing extends Component {
                             </nav>
                         </div>
                     </div>
-                    
-                    
                 </section>
             </React.Fragment>
         )
